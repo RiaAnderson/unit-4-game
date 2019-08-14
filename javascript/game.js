71d597bbce5b309057ff641f1fa6cd59b3667a35
Ria <astoriaa92@gmail.com>
2019-08-13 19:50:44 -0700

    added pseudocode to game file

---
+++ javascript/game.js
@@ -0,0 +1,13 @@
+//create random number for score
+
+//create random number for each crystal
+
+//counter for numbers added from crystal clicks
+
+//check to see if payer matches random number
+
+//counter for wins
+
+//check to see if payer goes above random number
+
+//counter for loses
