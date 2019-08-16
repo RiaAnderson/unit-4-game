1220e22e83d5d309c4740b7f0742f59e370336d9
Ria <astoriaa92@gmail.com>
2019-08-15 20:16:27 -0700

    created counter to update user score after crystal is clicked

--- javascript/game.js
+++ javascript/game.js
@@ -1,25 +1,40 @@
 //start game
-var winCounter =[];
+
 var loseCounter = [];
 
 //create random number for score
-var randomNumber = Math.floor((Math.random() * 120) +19);
+function randomNumber (){
+    var randomNumber = Math.floor((Math.random() * 120) +19);
     console.log(randomNumber);
+    $("#randomNumber").text(randomNumber);
+};
 
-//create random number for each crystal
-var crystalNumber = Math.floor ((Math.random() * 12) +1);
-    console.log(crystalNumber);
+randomNumber ();
 
-//Crystal button
-$("#oneCrystal").on("click", function(){
-    alert("test");
-    console.log();
-})
+// //Crystal button
+// $("#oneCrystal").on("click", function(){
+//     //create random number for each crystal
+// var crystalNumber = Math.floor ((Math.random() * 12) +1);
+// console.log(crystalNumber);
+// $("#crystalNumber").text(crystalNumber);
+// })
+
 //counter for numbers added from crystal clicks
-
+//counter +=crystalValue;
 //check to see if payer matches random number
 
 //counter for wins
+var winCounter = 0;
+//Crystal button
+var counter = 0;
+$("#oneCrystal").on("click", function(){
+    //create random number for each crystal
+var crystalNumber = Math.floor ((Math.random() * 12) +1);
+console.log(crystalNumber);
+    //add crystal total together
+    counter += crystalNumber;
+    $("#crystalNumber").text(counter);
+})
 
 //check to see if payer goes above random number
 
