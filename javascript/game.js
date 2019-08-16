2003cc397ba0871b88d690b98df83cfbb244d185
Ria <astoriaa92@gmail.com>
2019-08-15 21:00:13 -0700

    created if else to tell player if they win or lose

--- javascript/game.js
+++ javascript/game.js
@@ -1,6 +1,6 @@
 //start game
 
-var loseCounter = [];
+
 
 //create random number for score
 function randomNumber (){
@@ -25,19 +25,34 @@ randomNumber ();
 
 //counter for wins
 var winCounter = 0;
+
+//counter for loses
+var lossCounter = 0;
+
 //Crystal button
 var counter = 0;
 $("#oneCrystal").on("click", function(){
     //create random number for each crystal
 var crystalNumber = Math.floor ((Math.random() * 12) +1);
 console.log(crystalNumber);
+
     //add crystal total together
     counter += crystalNumber;
     $("#crystalNumber").text(counter);
-})
 
-//check to see if payer goes above random number
 
-//counter for loses
+//check to see if payer equals or goes above random number
+if (counter === randomNumber) {
+    
+    $("#winsLosses").text("You won!");
+    winCounter += 1;
+}
+    else if (counter >= randomNumber){
+    $("#winsLosses").text("You lost!")
+    alert("test");
+    lossCounter += 1;
+}
+});
+
 
 //restart the game after each lose or win
