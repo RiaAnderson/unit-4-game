c1d7f18e22f0dd560f9659d89e72c4087ce8a9e9
Ria <astoriaa92@gmail.com>
2019-08-15 19:06:46 -0700

    created random numbers and crystal image button

--- javascript/game.js
+++ javascript/game.js
@@ -1,7 +1,20 @@
+//start game
+var winCounter =[];
+var loseCounter = [];
+
 //create random number for score
+var randomNumber = Math.floor((Math.random() * 120) +19);
+    console.log(randomNumber);
 
 //create random number for each crystal
+var crystalNumber = Math.floor ((Math.random() * 12) +1);
+    console.log(crystalNumber);
 
+//Crystal button
+$("#oneCrystal").on("click", function(){
+    alert("test");
+    console.log();
+})
 //counter for numbers added from crystal clicks
 
 //check to see if payer matches random number
@@ -11,3 +24,5 @@
 //check to see if payer goes above random number
 
 //counter for loses
+
+//restart the game after each lose or win
