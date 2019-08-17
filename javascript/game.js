c6600b09cb99be1573f25f88df0355c28d7771eb
Ria <astoriaa92@gmail.com>
2019-08-16 23:13:12 -0700

    added new crystal image and created a separate function for checking score

--- javascript/game.js
+++ javascript/game.js
@@ -3,13 +3,13 @@
 
 
 //create random number for score
-function randomNumber (){
-    var randomNumber = Math.floor((Math.random() * 120) +19);
+function randomNumber() {
+    var randomNumber = Math.floor((Math.random() * 120) + 19);
     console.log(randomNumber);
     $("#randomNumber").text(randomNumber);
 };
 
-randomNumber ();
+randomNumber();
 
 // //Crystal button
 // $("#oneCrystal").on("click", function(){
@@ -29,30 +29,40 @@ var winCounter = 0;
 //counter for loses
 var lossCounter = 0;
 
-//Crystal button
+//score counter
 var counter = 0;
-$("#oneCrystal").on("click", function(){
+
+//Number for user to match
+var randomNumber;
+
+//crystal numbers
+var crystalNumber;
+
+//Crystal buttons
+$("#purpleCrystal").on("click", function () {
+    counter = counter + crystalNumber[0];
+
     //create random number for each crystal
-var crystalNumber = Math.floor ((Math.random() * 12) +1);
-console.log(crystalNumber);
+    // crystalNumber = Math.floor((Math.random() * 12) + 1);
+    // console.log(crystalNumber);
 
     //add crystal total together
     counter += crystalNumber;
     $("#crystalNumber").text(counter);
 
 
-//check to see if payer equals or goes above random number
-if (counter === randomNumber) {
-    
-    $("#winsLosses").text("You won!");
-    winCounter += 1;
-}
-    else if (counter >= randomNumber){
-    $("#winsLosses").text("You lost!")
-    alert("test");
-    lossCounter += 1;
-}
-});
+    //check to see if payer equals or goes above random number
+    function checkScore () {
+    if (counter === randomNumber) {
+        $("#winsLosses").text("You won!");
+        winCounter += 1;
+    } 
+    else if (counter >= randomNumber) {
+        $("#winsLosses").text("You lost!")
+        alert("test");
+        lossCounter = lossCounter + 1;
+    }
+};
 
 
 //restart the game after each lose or win
