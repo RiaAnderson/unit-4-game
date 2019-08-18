c612217e6725302d3b82a2ae5d3918972344a46e
Ria <astoriaa92@gmail.com>
2019-08-17 17:56:59 -0700

    updated css design

--- javascript/game.js
+++ javascript/game.js
@@ -22,57 +22,56 @@ $("#purpleCrystal").on("click", function () {
     // $("#crystalNumber").text(crystalNumber);
     checkScore();
     updateHTML()
-    
+
 });
 
-$("#pinkCrystal").on("click", function(){
+$("#pinkCrystal").on("click", function () {
     counter = counter + crystalNumber[1];
     checkScore();
     updateHTML()
-    
+
 });
 
-$("#greenCrystal").on("click", function(){
+$("#greenCrystal").on("click", function () {
     counter = counter + crystalNumber[2];
     checkScore();
     updateHTML()
-    
+
 });
 
-$("#greyCrystal").on("click", function (){
+$("#greyCrystal").on("click", function () {
     counter = counter + crystalNumber[3];
     checkScore();
     updateHTML()
-    
+
 });
 
 //function to start game
-    function initializeGame() {
+function initializeGame() {
 
     counter = 0;
 
-//create random number for score
+    //create random number for score
     randomNumber = Math.floor((Math.random() * 120) + 19);
     console.log(randomNumber);
     $("#randomNumber").text(randomNumber);
 
-//create random number for each crystal
-    crystalNumber = [Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) +1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1)];
+    //create random number for each crystal
+    crystalNumber = [Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1)];
     console.log(crystalNumber);
     $("#crystalNumber").text(counter);
 };
-    
-    
+
+
 
 
 //check to see if payer equals or goes above random number
-function checkScore () {
+function checkScore() {
     if (counter === randomNumber) {
         $("#winsLosses").text("You won!");
         winCounter += 1;
         initializeGame()
-    } 
-    else if (counter > randomNumber) {
+    } else if (counter > randomNumber) {
         $("#winsLosses").text("You lost!");
         lossCounter = lossCounter + 1;
         initializeGame()
@@ -82,8 +81,8 @@ function checkScore () {
 //print to html updated counters
 function updateHTML() {
     document.getElementById("crystalNumber").innerHTML = counter;
-    document.getElementById("winCounter").innerHTML = winCounter;
-    document.getElementById("lossCounter").innerHTML = lossCounter;
+    document.getElementById("winCounter").innerHTML = "Wins: " + winCounter;
+    document.getElementById("lossCounter").innerHTML = "losses: " + lossCounter;
 };
 
 //start game
